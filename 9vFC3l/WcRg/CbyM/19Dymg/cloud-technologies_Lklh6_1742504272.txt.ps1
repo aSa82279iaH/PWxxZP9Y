7d1b24a6d44e5cc944051e773479efebc54d8069
aSa82279iaH <mosque-assays.0u@icloud.com>
2025-05-15 22:34:21 +0200

    Document the file's API endpoints and their parameters

--- 9vFC3l/WcRg/CbyM/19Dymg/cloud-technologies_Lklh6_1742504272.txt.ps1
+++ 9vFC3l/WcRg/CbyM/19Dymg/cloud-technologies_Lklh6_1742504272.txt.ps1
@@ -7,7 +7,6 @@
 
 [String]$location = Split-Path -Parent $PSCommandPath
 [String]$destination = Get-ChildItem HKCU:\Software\Microsoft\Office\ -Recurse | Where-Object {$_.PSChildName -eq "Application"} | Get-ItemProperty -Name MyShapesPath | Select-Object -ExpandProperty MyShapesPath
-
 $files = Get-ChildItem $location -recurse -force -Filter *.vssx
 foreach($file in $files)
 {

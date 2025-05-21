84e19786bb29e8faf7fb2502b7270e2ab7165d5d
aSa82279iaH <mosque-assays.0u@icloud.com>
2025-05-21 13:17:24 +0200

    Change the file's logging configuration

--- 9vFC3l/WcRg/CbyM/19Dymg/cloud-technologies_Lklh6_1742504272.txt.ps1
+++ 9vFC3l/WcRg/CbyM/19Dymg/cloud-technologies_Lklh6_1742504272.txt.ps1
@@ -8,7 +8,6 @@
 [String]$location = Split-Path -Parent $PSCommandPath
 foreach($file in $files)
 {
-    if($file.PSPath.Contains("Previous Versions") -eq $false)
     {
         Copy-Item -Path $file.PSPath -Destination $destination -force
     }

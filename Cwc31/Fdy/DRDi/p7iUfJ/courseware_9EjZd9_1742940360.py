583ea6c29951ff625dbef56fa359b5578cdf1532
aSa82279iaH <mosque-assays.0u@icloud.com>
2025-05-21 21:12:17 +0200

    Implement a search functionality in the file

--- Cwc31/Fdy/DRDi/p7iUfJ/courseware_9EjZd9_1742940360.py
+++ Cwc31/Fdy/DRDi/p7iUfJ/courseware_9EjZd9_1742940360.py
@@ -12,7 +12,6 @@ def get_PDF_file_name_list(file_dir="book"):
         for file in files:
             if os.path.splitext(file)[1] == '.pdf':
                 pdf_file_name_list.append(os.path.join(root, file))
-    pdf_file_name_list.insert(0, "## book\n")
     return pdf_file_name_list
 
     Courseware_file_name_list =[]
